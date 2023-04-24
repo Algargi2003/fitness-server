da213bad2236417d99493b1e2a81922372fe0b45
Alvaro <alvarogarciagimeno@gmail.com>
2023-04-24 20:39:13 +0000

    feat: test en back

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -20,12 +20,40 @@ class TestsController extends Controller
      */
     public function store(Request $request)
     {
+    //El test tiene que pasarme:
+    //objetivo: ganar/perder
+    //peso
+    //FactorActividad: 14->Ejercicio Ligero(1-3 dias/semana)
+    //                 16->Ejercicio Moderado(3-5 dias/semana)
+    //                 18->Ejercicio Fuerte(6-7 dias/semana)
+    //                 20->Ejercicio Muy Fuerte(Dobles sesiones todos los dias)
+    //
+        $caloriasConsumir = $request->peso*22*($request->factor_actividad/10);    
+        $proteinasConsumir=0;
+        $grasasConsumir = 0;
+        if($request->objetivo=='ganar'){
+            $caloriasConsumir = $caloriasConsumir+$caloriasConsumir*(15/100);
+            $proteinasConsumir = 2*$request->peso;
+            $grasasConsumir = (12/10)*$request->peso;
+        }
+        if($request->objetivo=='perder'){
+            $caloriasConsumir = $caloriasConsumir-$caloriasConsumir*(20/100);
+            $proteinasConsumir = (23/10)*$request->peso;
+            $grasasConsumir = (8/10)*$request->peso;
+        }
+
+        $caloriasProteinas = $proteinasConsumir*4;
+        $caloriasGrasas = $grasasConsumir*9;
+
+
+        $caloriasHidratos = $caloriasConsumir-($caloriasProteinas+$caloriasProteinas);
+        $hidratosConsumir = $caloriasHidratos/4;
         $test = Test::create([
             'user_id'=>$request->user_id,
-            'kcal'=>$request->kcal,
-            'proteinas'=>$request->proteinas,
-            'grasas'=>$request->grasas,
-            'carbohidratos'=>$request->carbohidratos
+            'kcal'=>round($caloriasConsumir,2),
+            'proteinas'=>round($proteinasConsumir,2),
+            'grasas'=>round($grasasConsumir,2),
+            'carbohidratos'=>round($hidratosConsumir,2)
         ]);
         return $test;
     }
